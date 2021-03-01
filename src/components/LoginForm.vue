e492c99926cf8efa67e41fbff4a5103ff0dbfe8d
Leandro Koller <lekoller@yahoo.com>
2021-03-01 10:07:29 -0300

    feat: Homepage visuals ready.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,55 +1,39 @@
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
-        </v-col>
+  <div class="container">
+    <md-field md-clearable>
+      <label>Cleareable</label>
+      <md-input v-model="initial"></md-input>
+    </md-field>
 
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
-        </v-col>
+    <md-field>
+      <label>Password toggle</label>
+      <md-input v-model="password" type="password"></md-input>
+    </md-field>
 
-        <v-col cols="12" md="4">
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+    <md-field :md-toggle-password="false">
+      <label>Password field without toggle</label>
+      <md-input v-model="password" type="password"></md-input>
+    </md-field>
+  </div>
 </template>
 
 <script>
 export default {
+  name: "InlineActions",
   data: () => ({
-    valid: false,
-    firstname: "",
-    lastname: "",
-    nameRules: [
-      (v) => !!v || "Name is required",
-      (v) => v.length <= 10 || "Name must be less than 10 characters",
-    ],
-    email: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+/.test(v) || "E-mail must be valid",
-    ],
+    initial: "Initial Value",
+    password: "mysecurepassword",
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+.md-field:last-child {
+  margin-bottom: 40px;
+}
+
+.container {
+  width: 100%;
+  height: fit-content;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,29 @@
       <transition class="animate__animated animate__bounceIn">
         <img id="logo" alt="Prototicon logo" src="../assets/logo.png" />
       </transition>
-      <transition
-        class="animate__animated animate__fadeInRight animate__delay-2s"
-      >
+      <transition class="animate__animated animate__fadeInUp animate__delay-2s">
         <div class="login">
           <h2>Login</h2>
-          <LoginForm />
+          <div class="form_container">
+            <form @submit.prevent="">
+              <input
+                type="text"
+                name="username"
+                placeholder="username"
+                v-model="initial"
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                v-model="password"
+              />
+              <div class="anchor_and_button">
+                <a href="">Sign up</a>
+                <button>Login</button>
+              </div>
+            </form>
+          </div>
         </div>
       </transition>
     </div>
@@ -31,11 +48,15 @@ import LoginForm from "../components/LoginForm";
 export default {
   name: "Home",
   setup() {
+    let initial = "";
+    let password = "";
+
     const state = reactive({
-      loaded: false,
+      initial: "",
+      password: "",
     });
 
-    return state, LoginForm;
+    return state, LoginForm, initial, password;
   },
 };
 </script>
@@ -56,8 +77,10 @@ h2 {
 .container {
   color: white;
   background-color: #b08cfa;
+  width: 100%;
   height: 100vh;
   position: relative;
+  margin: 0;
 
   @supports (backdrop-filter: blur()) {
     .welcome {
@@ -65,17 +88,17 @@ h2 {
       left: 8vw;
       top: 22vh;
       max-width: 26vw;
+      padding: 10px;
+      border-radius: 8px;
       backdrop-filter: blur(8px);
       z-index: 1;
     }
 
     .login {
       backdrop-filter: blur(8px);
-      backdrop-filter: grayscale(20%);
-      backdrop-filter: brightness(80%);
       position: absolute;
       border-radius: 8px;
-      right: 8vw;
+      right: 6vw;
       top: 14vh;
       max-width: 26vw;
       padding: 20px;
@@ -83,6 +106,58 @@ h2 {
       h2 {
         font-size: 2.4rem;
         margin-top: 0;
+      }
+    }
+  }
+}
+
+.form_container {
+  width: 100%;
+  height: fit-content;
+  font-family: "Roboto";
+  font-weight: 100;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    input {
+      margin: 8px;
+      border-radius: 8px;
+      padding: 0.2rem;
+      padding-left: 10px;
+      outline: none;
+      background-color: white;
+      color: #4bb6ff;
+    }
+
+    .anchor_and_button {
+      display: flex;
+      justify-content: space-between;
+      padding: 8px;
+
+      a {
+        color: #73e3e7;
+      }
+
+      button {
+        outline: none;
+        border: none;
+        border-radius: 8px;
+        background-color: white;
+        color: #b08cfa;
+        padding: 4px 12px 4px 12px;
+        cursor: pointer;
+        transition: all 0.25s ease;
+
+        &:hover {
+          transform: scale(1.05, 1.05);
+          color: #73e3e7;
+        }
+
+        &:active {
+          transform: scale(0.8, 0.8);
+        }
       }
     }
   }
